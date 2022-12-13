262427ff854840edc5d4b4bb9d5c17e47f7da4a5
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-13 22:07:17 +0300

    Created Example005_if_else

---
+++ Example/Example005_If_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
