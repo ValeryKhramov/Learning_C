66a4a5508a3850c98b6bed331b6037b3fecb2012
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-17 20:44:19 +0300

    Finded max for nine numbers with array

---
+++ Example/Example009_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+Clear();
+
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {11, 12, 13, 14, 15, 16, 17, 18, 19};
+ 
+
+int result = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]) );
+WriteLine(result);
+
+
+
+
