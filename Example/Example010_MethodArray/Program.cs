6666f7f8043425303160b5ea00beb368d79422d0
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-17 20:52:19 +0300

    Created MethodArray find index on number in array

---
+++ Example/Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+
+int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        WriteLine(index);
+        break;
+    }
+
+    index++;
+}
